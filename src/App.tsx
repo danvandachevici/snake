ffd22d04727e2ccc74b5fceefeff81273acdc2ab
Dan VANDACHEVICI <dan@vandachevici.ro>
2021-12-12 21:42:37 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Board from './components/Board/Board';
+
+function App() {
+  return (
+    <Board />
+  );
+}
+
+export default App;
---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,6 @@
+export default function Board() {
+  return (
+    <div>
+    </div>
+  )
+}
